0cd23d85242e60ba0b1d3836040abe5792c0b44e
Alina T <alina1989malina@yandex.ru>
2021-01-21 23:05:19 +0300

    init

---
+++ mysnaketestlibrary/src/main/java/android/example/mysnaketestlibrary/Test.java
@@ -0,0 +1,9 @@
+package android.example.mysnaketestlibrary;
+
+import android.util.Log;
+
+public class Test {
+    public void hello() {
+        Log.d("TAG", "HELLO FROM FIRST SNAKE LIBRARY");
+    }
+}
